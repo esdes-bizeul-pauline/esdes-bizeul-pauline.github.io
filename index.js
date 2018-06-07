38953d0f315eae8527eddbe56fef883b1f91eaa7
esdes-bizeul-pauline <32122382+esdes-bizeul-pauline@users.noreply.github.com>
2018-06-07 09:21:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+
+
+anime({
+  targets: '#about-me',
+  translateY: [
+    { value: -200, duration: 800 },
+    { value: 0, duration: 800 },
+  ],
+  opacity: [
+    { value: 0, duration: 800 },
+    { value: 1, duration: 800 },
+  ],
+});
+
+
+anime({
+  targets: '#pauline',
+  translateY: [
+    { value: -500, duration: 800 },
+    { value: 0, duration: 800 },
+  ],
+  opacity: [
+    { value: 0, duration: 800 },
+    { value: 1, duration: 800 },
+  ],
+});
+
+anime({
+  targets: '#london',
+  translateY: [
+    { value: -700, duration: 800 },
+    { value: 0, duration: 800 },
+  ],
+  opacity: [
+    { value: 0, duration: 800 },
+    { value: 1, duration: 800 },
+  ],
+});
+
+anime({
+  targets: '#moi',
+  translateY: [
+    { value: -200, duration: 800 },
+    { value: 0, duration: 800 },
+  ],
+  opacity: [
+    { value: 0, duration: 800 },
+    { value: 1, duration: 800 },
+  ],
+});
+
+anime({
+  targets: '#keep',
+  translateY: [
+    { value: -200, duration: 800 },
+    { value: 0, duration: 800 },
+  ],
+  opacity: [
+    { value: 0, duration: 800 },
+    { value: 1, duration: 800 },
+  ],
+});
+
+
+anime({
+  targets: '#formation',
+  translateY: [
+    { value: -200, duration: 800 },
+    { value: 0, duration: 800 },
+  ],
+  opacity: [
+    { value: 0, duration: 800 },
+    { value: 1, duration: 800 },
+  ],
+});
